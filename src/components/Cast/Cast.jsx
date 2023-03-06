d27916fff055a87a9395e63c3ba66021d51a6ea0
Daniel <daniil.spectr@gmail.com>
2023-03-06 23:30:03 +0200

    add lazy loading

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -4,37 +4,41 @@ import { getCastList } from 'components/utils';
 import { List } from './Cast.styled';
 const Cast = () => {
   const [cast, setCast] = useState(null);
-
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
   const imgUrl = 'https://image.tmdb.org/t/p/w500/';
   useEffect(() => {
+    setLoading(true);
     getCastList(id).then(res => {
       setCast(res.cast.slice(0, 10));
+      setLoading(false);
     });
   }, [id]);
 
   return (
     <>
-      {cast && (
-        <List>
-          {cast.map(({ cast_id, name, character, profile_path }) => {
-            return (
-              <li key={cast_id}>
-                <img
-                  src={
-                    profile_path
-                      ? `${imgUrl}${profile_path}`
-                      : `https://m.media-amazon.com/images/S/sash/9FayPGLPcrscMjU.png`
-                  }
-                  alt="text"
-                ></img>
-                <p>{name}</p>
-                <p>Character: {character}</p>
-              </li>
-            );
-          })}
-        </List>
-      )}
+      {loading
+        ? 'Loading...'
+        : cast && (
+            <List>
+              {cast.map(({ cast_id, name, character, profile_path }) => {
+                return (
+                  <li key={cast_id}>
+                    <img
+                      src={
+                        profile_path
+                          ? `${imgUrl}${profile_path}`
+                          : `https://m.media-amazon.com/images/S/sash/9FayPGLPcrscMjU.png`
+                      }
+                      alt="text"
+                    ></img>
+                    <p>{name}</p>
+                    <p>Character: {character}</p>
+                  </li>
+                );
+              })}
+            </List>
+          )}
     </>
   );
 };
